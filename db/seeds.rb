81255370bef780bbde3af9b0ccf70409e5678c88
yulong_052 <yulong_052@van-macmini-wireless.smart.icitymobile.com>
2019-07-18 15:39:56 +0800

    store

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(:email =>"850197942@qq.com",:password => "yu123456",:reset_password_token=> "yu123456",:is_admin => true)

28091213e2491109ac92071ce4f89d44e8ca82e3
yulong_052 <yulong_052@van-macmini-wireless.smart.icitymobile.com>
2019-07-19 13:11:41 +0800

    store

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  resources :cart_items
+
+  resources :carts do
+    collection do
+      delete :clean
+    end
+  end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
@@ -6,5 +13,10 @@ Rails.application.routes.draw do
   resources :products
   end
   root 'products#index'
-  resources :products
+  resources :products do
+      member do
+        post :add_to_cart
+      end
+  end
+
 end

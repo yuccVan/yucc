b30c0d3e2f439b55390c14ead117256fc85e9b2a
yulong_052 <yulong_052@van-macmini-wireless.smart.icitymobile.com>
2019-07-25 16:31:52 +0800

    store

--- config/application.rb
+++ config/application.rb
@@ -5,13 +5,13 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.time_zone = 'Beijing'
-config.active_record.default_timezone = :local
+
 module Store
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.time_zone = 'Beijing'
+    config.active_record.default_timezone = :local
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading

2faefd84b28493495c0e921a53753de60d561feb
yulong_052 <yulong_052@van-macmini-wireless.smart.icitymobile.com>
2019-07-26 11:07:03 +0800

    store

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,12 @@
 CarrierWave.configure do |config|
-  config.storage             = "qiniu"
+  config.storage             = :qiniu
   config.qiniu_access_key    = "AH-MwvsBkhVKR_XOe7TI5hTyswyieH475x09-vAo"
   config.qiniu_secret_key    = "k5YT1bKokr--oFgLczrIv4r9Cp5EiHjgjkIRuA8H"
   config.qiniu_bucket        = "yucc"
   config.qiniu_bucket_domain = "pv1c4q8dr.bkt.clouddn.com"
   config.qiniu_block_size    = 4*1024*1024
-  #config.qiniu_protocol      = "http"
+  config.qiniu_protocol      = "http"
+  config.qiniu_bucket_private = false
   #config.qiniu_up_host       = "http://up.qiniu.com"  #选择不同的区域时，"up.qiniu.com" 不同
 
 end
